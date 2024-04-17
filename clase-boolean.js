d9ff1e23b1c31026581c70d37e8477c6a849be9d
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2024-04-17 17:42:55 -0600

    CONVERSION A BOOLEANOS

---
+++ clase-boolean.js
@@ -0,0 +1,15 @@
+const isActive = true
+const hasPermission = false
+
+// Convercion implicita
+const resultado = 5 > 3
+console.log(resultado)
+
+const name = 'MArvin'
+console.log(!!name)
+
+// Convercion explicita
+const value = 0
+const otherValue = -24
+const explicitBoolean = Boolean(otherValue)
+console.log(explicitBoolean)
